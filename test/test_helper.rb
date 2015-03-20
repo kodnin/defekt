b7e2ccbf7ff78572acc050b58c24ff69019f9466
David Boot <kodnin@gmail.com>
2015-03-20 19:29:14 +0100

    add stub helper to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 
 require 'defekt'
 require 'minitest/autorun'
+
+class Minitest::Test
+  def stub(object, methot, value)
+    object.instance_eval { define_singleton_method(methot) { value } }
+  end
+end
