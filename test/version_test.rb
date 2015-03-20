80b484296d7249bc384eb7679a504aadef3a2723
David Boot <kodnin@gmail.com>
2015-03-20 13:23:35 +0100

    namespace version test for consistency

--- test/version_test.rb
+++ test/version_test.rb
@@ -1,6 +1,6 @@
 require_relative 'test_helper'
 
-class VersionTest < Minitest::Test
+class Defekt::VersionTest < Minitest::Test
   def test_version
     assert_equal '0.0.2', Defekt::VERSION
   end
