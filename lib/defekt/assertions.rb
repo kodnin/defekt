e31aecbc544193673a0a5bf9130cb537d9c3f78e
David Boot <kodnin@gmail.com>
2015-03-22 15:25:30 +0100

    add message to assertions

--- lib/defekt/assertions.rb
+++ lib/defekt/assertions.rb
@@ -1,27 +1,33 @@
 module Defekt
   module Assertions
-    def true!(boolean)
-      unless boolean
-        raise Errors::TrueError, "|#{boolean}| is not |true|"
+    def true!(value)
+      unless value
+        raise Errors::TrueError, message(value, 'is not', true)
       end
     end
 
     def equal_to!(expected, actual)
       unless expected == actual
-        raise Errors::EqualToError, "|#{actual}| is not equal to |#{expected}|"
+        raise Errors::EqualToError, message(actual, 'is not equal to', expected)
       end
     end
 
     def included_in!(collection, member)
       unless collection.include?(member)
-        raise Errors::IncludedInError, "|#{member}| is not included in |#{collection}|"
+        raise Errors::IncludedInError, message(member, 'is not included in', collection)
       end
     end
 
     def instance_of!(klass, instance)
       unless instance.instance_of?(klass)
-        raise Errors::InstanceOfError, "|#{instance}| is not an instance of |#{klass}|"
+        raise Errors::InstanceOfError, message(instance, 'is not an instance of', klass)
       end
+    end
+
+    private
+
+    def message(object1, text, object2)
+      "~#{object1}~ #{text} #{object2}"
     end
   end
 end
--- test/assertions_test.rb
+++ test/assertions_test.rb
@@ -13,25 +13,25 @@ class Defekt::AssertionsTest < Minitest::Test
 
   def test_true!
     e = assert_raises(Defekt::Errors::TrueError) { @object.true!(false) }
-    assert_equal '|false| is not |true| (TrueError)', e.message
+    assert_equal '~false~ is not true (TrueError)', e.message
     assert_nil @object.true!(true)
   end
 
   def test_equal_to!
     e = assert_raises(Defekt::Errors::EqualToError) { @object.equal_to!(1, 2) }
-    assert_equal '|2| is not equal to |1| (EqualToError)', e.message
+    assert_equal '~2~ is not equal to 1 (EqualToError)', e.message
     assert_nil @object.equal_to!(1, 1)
   end
 
   def test_included_in!
     e = assert_raises(Defekt::Errors::IncludedInError) { @object.included_in!([], 1) }
-    assert_equal '|1| is not included in |[]| (IncludedInError)', e.message
+    assert_equal '~1~ is not included in [] (IncludedInError)', e.message
     assert_nil @object.included_in!([1], 1)
   end
 
   def test_instance_of!
     e = assert_raises(Defekt::Errors::InstanceOfError) { @object.instance_of!(String, 1) }
-    assert_equal '|1| is not an instance of |String| (InstanceOfError)', e.message
+    assert_equal '~1~ is not an instance of String (InstanceOfError)', e.message
     assert_nil @object.instance_of!(String, '1')
   end
 end
