efb15f146fac57568ae8e259d83bcb5c2a5a1228
David Boot <kodnin@gmail.com>
2015-03-27 11:06:06 +0100

    bump version

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require_relative 'test_helper'
 
 class Defekt::VersionTest < Minitest::Test
   def test_version
-    assert_equal '0.0.3', Defekt::VERSION
+    assert_equal '0.0.4', Defekt::VERSION
   end
 end
