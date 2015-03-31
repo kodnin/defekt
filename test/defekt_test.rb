5f417060e613393bfbe9a98d1e2ee8380af560ab
David Boot <kodnin@gmail.com>
2015-04-01 01:15:42 +0200

    mock runner in defekt run test

--- test/defekt_test.rb
+++ test/defekt_test.rb
@@ -3,5 +3,12 @@ require_relative 'test_helper'
 class DefektTest < Minitest::Test
   def test_run
     assert_respond_to Defekt, :run
+    mock = MiniTest::Mock.new
+    mock.expect(:run, mock)
+    mock.expect(:report, mock)
+    load File.expand_path('../../lib/defekt/object.rb', __FILE__) # FIXME
+    Defekt::Runner.stub(:new, mock) { Defekt.run }
+    load 'minitest/mock.rb' # FIXME
+    assert mock.verify
   end
 end
